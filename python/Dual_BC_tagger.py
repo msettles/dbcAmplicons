aec33f5abea9ef5263d275d572c185217f316d79
msettles <msettles@uidaho.edu>
2013-12-14 07:45:08 -0800

    testing

--- python/Dual_BC_tagger.py
+++ python/Dual_BC_tagger.py
@@ -17,10 +17,10 @@ barcodeFile = "MetaData/barcodeLookupTable.txt"
 primerForward = "MetaData/fwd_primer.fasta"
 primerReverse = "MetaData/rev_primer.fasta"
 
-R1_File ='Amplicon_Raw_fastq/20130910DBCAmpliconWesleyForney/AllReads_NoIndex_L001_R1_001.fastq.gz'
-R2_File ='Amplicon_Raw_fastq/20130910DBCAmpliconWesleyForney/AllReads_NoIndex_L001_R2_001.fastq.gz'
-R3_File ='Amplicon_Raw_fastq/20130910DBCAmpliconWesleyForney/AllReads_NoIndex_L001_R3_001.fastq.gz'
-R4_File ='Amplicon_Raw_fastq/20130910DBCAmpliconWesleyForney/AllReads_NoIndex_L001_R4_001.fastq.gz'
+R1_File ='Amplicon_Raw_fastq/test40k_R1_001.fastq.gz'
+R2_File ='Amplicon_Raw_fastq/test40k_R2_001.fastq.gz'
+R3_File ='Amplicon_Raw_fastq/test40k_R3_001.fastq.gz'
+R4_File ='Amplicon_Raw_fastq/test40k_R4_001.fastq.gz'
 
 Output_prefix = 'DBCWesleyForney'
 
