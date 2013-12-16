4f6b00f14dee18c4cf3555e136d6a9b4beba3020
msettles <msettles@uidaho.edu>
2013-12-16 15:23:29 -0800

    created method for barcode id

--- python/Dual_BC_tagger.py
+++ python/Dual_BC_tagger.py
@@ -216,47 +216,47 @@ try:
         reads += 1
 
         # ### Barcode One Matching ###
-        # bc1 = None
-        # bc1Mismatch = False
-        # if read2.seq.tostring() in bcTable.P7:
-        #     bc1 = read2.seq.tostring()
-        # else:
-        #     for key in bcTable.P7:
-        #         bcdist = barcodeDist(key, read2.seq.tostring())
-        #         if bcdist <= barcodeMaxDiff:
-        #             bc1 = key
-        #             bc1Mismatch = True
+        bc1 = None
+        bc1Mismatch = False
+        if read2.seq.tostring() in bcTable.P7:
+            bc1 = read2.seq.tostring()
+        else:
+            for key in bcTable.P7:
+                bcdist = barcodeDist(key, read2.seq.tostring())
+                if bcdist <= barcodeMaxDiff:
+                    bc1 = key
+                    bc1Mismatch = True
 
-        # ### Barcode Two Matching ###
-        # bc2 = None
-        # bc2Mismatch = False
-        # if read3.seq.tostring() in bcTable.P5:
-        #     bc2 = read3.seq.tostring()
-        # else:
-        #     for key in bcTable.P5:
-        #         bcdist = barcodeDist(key, read3.seq.tostring())
-        #         if bcdist <= barcodeMaxDiff:
-        #             bc2 = key
-        #             bc2Mismatch = True
+        ### Barcode Two Matching ###
+        bc2 = None
+        bc2Mismatch = False
+        if read3.seq.tostring() in bcTable.P5:
+            bc2 = read3.seq.tostring()
+        else:
+            for key in bcTable.P5:
+                bcdist = barcodeDist(key, read3.seq.tostring())
+                if bcdist <= barcodeMaxDiff:
+                    bc2 = key
+                    bc2Mismatch = True
 
-        # ### Barcode Pair Matching ###
-        # combined_bc = None
-        # if "%s %s" % (bc1, bc2) in bcTable.barcodes:
-        #     combined_bc = bcTable.barcodes["%s %s" % (bc1, bc2)][0]
-        #     counters[combined_bc][0] += 1
-        #     if bc1Mismatch:
-        #         counters[combined_bc][1] += 1
-        #     if bc2Mismatch:
-        #         counters[combined_bc][2] += 1
-        bc = getBarcode(read2.seq.tostring(), read3.seq.tostring(),barcodeMaxDiff)
-        if (bc[0] != None):
-            counters[bc[0]][0] += 1
-            if bc[1] > 0:
-                counters[bc[0]][1] += 1
-            if bc[2] > 0:
-                counters[bc[0]][2] += 1
+        ### Barcode Pair Matching ###
+        combined_bc = None
+        if "%s %s" % (bc1, bc2) in bcTable.barcodes:
+            combined_bc = bcTable.barcodes["%s %s" % (bc1, bc2)][0]
+            counters[combined_bc][0] += 1
+            if bc1Mismatch:
+                counters[combined_bc][1] += 1
+            if bc2Mismatch:
+                counters[combined_bc][2] += 1
+        # bc = getBarcode(read2.seq.tostring(), read3.seq.tostring(),barcodeMaxDiff)
+        # if (bc[0] != None):
+        #     counters[bc[0]][0] += 1
+        #     if bc[1] > 0:
+        #         counters[bc[0]][1] += 1
+        #     if bc[2] > 0:
+        #         counters[bc[0]][2] += 1
 
-        combined_bc = bc[0]
+        # combined_bc = bc[0]
 
         ### Primer Matching ###
         primer1 = None
