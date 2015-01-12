aff06bd0b0f087bfa15a6b532f994620f63dfec8
Alida <alida@uidaho.edu>
2015-01-12 10:33:36 -0800

    updated version to v0.5.9-01122015

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ config = {
     'url': 'https://github.com/msettles/DBC_amplicons/py-dbcAmplicons',
     'download_url': 'https://github.com/msettles/DBC_amplicons/py-dbcAmplicons',
     'author_email': 'msettles@uidaho.edu',
-    'version': 'v0.5.8-1152014',
+    'version': 'v0.5.9-01122015',
     'install_requires': [],
     'packages': ['dbcAmplicons'],
     'scripts': ['bin/dbcAmplicons', 'scripts/python/convert2ReadTo4Read.py', 'scripts/R/reduce_amplicons.R'],
