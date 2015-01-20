07415a6c66cdee4d0c095f34850b6fd6c3795adb
Alida <alida@uidaho.edu>
2015-01-20 13:38:22 -0800

    updated test files

--- tests/test_dbAmplicons.sh
+++ tests/test_dbAmplicons.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
-RDP_PATH="~/opt/RDPTools/classifier/dist/classifier.jar"
+##RDP_PATH="~/opt/RDPTools/classifier/dist/classifier.jar"
 
 ## testing preprocess
 echo "Testing dbcAmplicons preprocess"
-dbcAmplicons preprocess -b 15001 -B barcodeLookupTable.txt -P primerLookupTable.txt -1 Amplicon_Raw_fastq/Test100K_16S_R1_001.fastq.gz Amplicon_Raw_fastq/test40k_R1_001.fastq.gz  -O preprocess/trimL --debug &> out.txt
+dbcAmplicons preprocess -b 15001 -B barcodeLookupTable.txt -P primerLookupTable.txt -1 Amplicon_Raw_fastq/Test100K_16S_R1_001.fastq.gz Amplicon_Raw_fastq/test40k_R1_001.fastq.gz -O preprocess/trimL --debug &> out.txt
 
 echo "Testing dbcAmplicons splitreads"
 dbcAmplicons splitreads -b 15001 -S sampleLookupTable.txt -1 preprocess/trimL_R1.fastq.gz -O splitreads --debug >> out.txt 2>&1
@@ -13,7 +13,7 @@ echo "Testing dbcAmplicons join"
 dbcAmplicons join -t 4 -x 0.25 -1 splitreads/match_twoprimer_R1.fastq.gz -O join/match_twoprimer >> out.txt 2>&1
 
 echo "Testing dbcAmplicons classify"
-dbcAmplicons classify -b 7500 -O join/classify -U join/match_twoprimer.extendedFrags.fastq.gz --debug  --rdpPath $RDP_PATH -p 4 -1 join/match_twoprimer.notCombined_1.fastq.gz -2 join/match_twoprimer.notCombined_2.fastq.gz >> out.txt 2>&1
+dbcAmplicons classify -b 7500 -O join/classify -U join/match_twoprimer.extendedFrags.fastq.gz --debug --rdpPath /home/alida/Documents/Idaho/bioinformatics/RDPTools/classifier/dist/classifier.jar -p 4 -1 join/match_twoprimer.notCombined_1.fastq.gz -2 join/match_twoprimer.notCombined_2.fastq.gz >> out.txt 2>&1
 
 echo "Testing dbcAmplicons abundance"
 dbcAmplicons abundance -O join/abundance -F join/classify.fixrank --debug >> out.txt 2>&1
@@ -30,11 +30,11 @@ splitReadsBySample.py -O allSamples -1 Amplicon_Raw_fastq/Hkates_R1.40k.fastq.gz
 echo "Testing splitReadsBySample single reads"
 splitReadsBySample.py -O allSamples -U Amplicon_Raw_fastq/Hkates_R1.40k.fastq.gz --debug >> out.txt 2>&1
 
-echo "DIFFERENCES BETWEEN TESTS AND EXPECTED"
-echo "######################################"
-diff out.txt expected.txt
-echo "######################################"
+#echo "DIFFERENCES BETWEEN TESTS AND EXPECTED"
+#echo "######################################"
+#diff out.txt expected.txt
+#echo "######################################"
 
-echo "Removing folders and files"
-rm -rf allSamples backtest join preprocess splitreads out.txt
+#echo "Removing folders and files"
+#rm -rf allSamples backtest join preprocess splitreads out.txt
 
