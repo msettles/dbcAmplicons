2bb9c5c6c9a5b0cfd938fd594fe978a6951e94a4
msettles <msettles@uidaho.edu>
2015-02-19 18:32:43 -0800

    change flash to flash2-amp and compile

--- cleandir.sh
+++ cleandir.sh
@@ -3,7 +3,8 @@ rm -rf tests/backtest
 rm -rf tests/join
 rm -rf tests/preprocess
 rm -rf tests/splitreads
-
+rm -rf tests/allSamples/
+rm -rf tests/out.txt
 rm -rf tests/Amplicon_Raw_fastq/*Unidentified*
 rm -rf tests/Amplicon_Raw_fastq/*Identified*
 rm -rf tests/Amplicon_Raw_fastq/*Barcodes*
@@ -13,3 +14,10 @@ rm -rf build
 rm -rf dbcAmplicons/*.pyc
 rm -rf dbcAmplicons/*.so
 rm -rf dbcAmplicons.egg-info
+
+rm -rf bin/flash2-amp
+
+cd included-apps/FLASH
+make clean
+
+cd ../..
