b85205b99e156524287984884c17300d7aa0c263
Matt Settles <mattsettles@gmail.com>
2017-09-05 10:11:38 -0700

    work on test script and version report

--- scripts/dbcVersionReport.sh
+++ scripts/dbcVersionReport.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env sh
 
-echo "Analysis version report created."
-
-touch version_report.txt
-printf "Report generated on $(date)\n" > version_report.txt
-dbcAmplicons --version >> version_report.txt 2>&1
-flash2 --version >> version_report.txt 2>&1
-java -jar $RDP_PATH version >> version_report.txt 2>&1
+printf "Report generated on $(date)\n"
+dbcAmplicons --version
+flash2 --version
+java -jar $RDP_PATH/classifier.jar version
